20b904157d2ecc878d9b5c8e379e23ed98b36f34
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-11-16 23:52:40 +0500

    nike starter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import components
+
+
+const App = () => {
+
+  return (
+   <></>
+  );
+};
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// logo
+
+
+// nike img
+
+
+// import framer hooks
+
+const Card = () => {
+
+  
+  return (
+    
+  )
+  
+};
+
+export default Card;
